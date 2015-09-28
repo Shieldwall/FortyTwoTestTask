f2f6e82e1c2a5185f7680fbfa056e395c688ce7e
shieldwall <muslyaster@gmail.com>
2015-09-28 15:21:32 +0300

    ticket:1 added User model to models, updated tests with a docstring, proper import and corrected datetime usage. Added fixtures dir, created a fixture of required info. Added FIXTURES_DIRS to settings

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    bio = models.TextField()
+    contacts = models.CharField(max_length=200)
--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django.test import TestCase
-from .models import *
+from .models import User
 # Create your tests here.
 
 
@@ -9,9 +9,12 @@ class HelloTest(TestCase):
     fixtures = ['hello']
 
     def test_fixture_with_personal_data(self):
+
+        '''test makes sure that the fixture with my personal info
+        has all info right'''
         s = User.objects.get(pk=1)
         self.assertEquals(s.name, "Alexey")
         self.assertEquals(s.surname, "Shevtsov")
-        self.assertEquals(s.date_of_birth, datetime.date('1989', '1', '2'))
+        self.assertEquals(s.date_of_birth, datetime.date(1989, 1, 2))
         self.assertEquals(s.bio, "living")
         self.assertEquals(s.contacts, "muslyaster@gmail.com")
