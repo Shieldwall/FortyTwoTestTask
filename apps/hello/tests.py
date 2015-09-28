1f355ca806f7034d71c12f917b45faa85be8baa0
shieldwall <muslyaster@gmail.com>
2015-09-28 14:39:34 +0300

    ticket:1 created tests for a fixture

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,17 @@
-from django.test import TestCase
+import datetime
 
+from django.test import TestCase
+from .models import *
 # Create your tests here.
 
 
-class SomeTests(TestCase):
-    def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 5)
+class HelloTest(TestCase):
+    fixtures = ['hello']
+
+    def test_fixture_with_personal_data(self):
+        s = User.objects.get(pk=1)
+        self.assertEquals(s.name, "Alexey")
+        self.assertEquals(s.surname, "Shevtsov")
+        self.assertEquals(s.date_of_birth, datetime.date('1989', '1', '2'))
+        self.assertEquals(s.bio, "living")
+        self.assertEquals(s.contacts, "muslyaster@gmail.com")
